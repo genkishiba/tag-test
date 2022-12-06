0c53065fe24d7a370923c6f0b8f28e8adf3413ab
emma <dogwolfpuma@gmail.com>
2022-12-06 21:29:50 +0800

    first

---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,46 @@
+class ShopsController < ApplicationController
+  def index
+    @shops = Shop.all
+  end
+
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(find_params)
+    if @shop.save
+      redirect_to shops_path, notice:"新增成功"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @shop = Shop.find_by!(id: params[:id])
+  end
+
+  def edit
+    @shop = Shop.find_by!(id: params[:id])
+  end
+
+  def update
+    @shop = Shop.find_by!(id: params[:id])
+    if @shop.update(find_params)
+      redirect_to shops_path, notice:"更改成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @shop = Shop.find_by!(id: params[:id])
+    @shop.destroy
+    redirect_to shops_path, notice:"刪除成功"
+  end
+
+  private
+  def find_params
+    params.require(:shop).permit(:title, :tel, :address)
+  end
+end
